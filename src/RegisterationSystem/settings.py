47d01ec78a06c47776cf869c3bfae0f10df7ba52
voidrank <voidrank@gmail.com>
2015-03-19 19:33:36 +0800

    random server key

--- src/RegisterationSystem/settings.py
+++ src/RegisterationSystem/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g)o7^bo6_yx!j(-lek)_4=o4bg0t4crf_k=dovjukyr!v(7*rg'
+SECRET_KEY = os.urandom(32)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
